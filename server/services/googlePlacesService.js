79e30ab73f5fe62969eae99e6c9cfdf2e082307a
ym-xu <yiming.tsui@gmail.com>
2024-06-02 21:29:58 +0800

    craw img and multiple outlet types

--- server/services/googlePlacesService.js
+++ server/services/googlePlacesService.js
@@ -4,33 +4,15 @@ const fileHandler = require('../utils/fileHandler');
 
 exports.getPlaces = async (lat, lon) => {
     console.log('Getting places near', lat, lon);
-    const response = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json`, {
-        params: {
-            location: `${lat},${lon}`,
-            radius: 300,
-            type: 'restaurant',
-            key: config.googleApiKey
-        },
-        proxy: {
-            host: '127.0.0.1',
-            port: 7890,
-            protocol: 'http'
-        }
-    });
+    const types = ['restaurant', 'cafe', 'bar', 'night_club', 'movie_theater'];
+    const allPlaces = [];
 
-    const places = response.data.results;
-    console.log('Places:', places);
-    for (const place of places) {
-        const placeDetails = {
-            name: place.name,
-            rating: place.rating,
-            address: place.vicinity,
-            place_id: place.place_id
-        };
-
-        const detailsResponse = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json`, {
+    for (const type of types) {
+        const response = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json`, {
             params: {
-                place_id: place.place_id,
+                location: `${lat},${lon}`,
+                radius: 1000,
+                type: type,
                 key: config.googleApiKey
             },
             proxy: {
@@ -40,14 +22,50 @@ exports.getPlaces = async (lat, lon) => {
             }
         });
 
-        const details = detailsResponse.data.result;
-        placeDetails.phone = details.formatted_phone_number;
-        placeDetails.opening_hours = details.opening_hours;
-        placeDetails.reviews = details.reviews;
-        console.log(placeDetails)
+        const places = response.data.results;
+        console.log(`${type} Places:`, places);
 
-        await fileHandler.savePlaceDetails(place.place_id, placeDetails);
+        for (const place of places) {
+            const placeDetails = {
+                name: place.name,
+                rating: place.rating,
+                address: place.vicinity,
+                type: type, 
+                place_id: place.place_id
+            };
+
+            const detailsResponse = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json`, {
+                params: {
+                    place_id: place.place_id,
+                    fields: 'formatted_phone_number,opening_hours,reviews,photos',
+                    key: config.googleApiKey
+                },
+                proxy: {
+                    host: '127.0.0.1',
+                    port: 7890,
+                    protocol: 'http'
+                }
+            });
+
+            const details = detailsResponse.data.result;
+            placeDetails.phone = details.formatted_phone_number;
+            placeDetails.opening_hours = details.opening_hours;
+            placeDetails.reviews = details.reviews;
+
+            if (details.photos && details.photos.length > 0) {
+                const photoReference = details.photos[0].photo_reference;
+                const photoUrl = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoReference}&key=${config.googleApiKey}`;
+                placeDetails.photoUrl = photoUrl;
+            }
+
+            console.log(`${type} Place Details:`, placeDetails);
+            await fileHandler.savePlaceDetails(placeDetails, type);
+            if (placeDetails.photoUrl) {
+                await fileHandler.savePhotoUrl(placeDetails.place_id, placeDetails.photoUrl);
+            }
+        }
+
+        allPlaces.push(...places);
     }
-
-    return places;
+    return allPlaces;
 };
--- server/utils/fileHandler.js
+++ server/utils/fileHandler.js
@@ -1,10 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 
-exports.savePlaceDetails = (placeId, placeDetails) => {
-    const dir = path.join(__dirname, '../../document_db');
+exports.savePlaceDetails = (placeDetails) => {
+    const dir = path.join(__dirname, '../../document_db', placeDetails.type);
     if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
+        fs.mkdirSync(dir, { recursive: true });
     }
-    fs.writeFileSync(path.join(dir, `${placeId}.json`), JSON.stringify(placeDetails, null, 2));
+    fs.writeFileSync(path.join(dir, `${placeDetails.place_id}.json`), JSON.stringify(placeDetails, null, 2));
+};
+
+exports.savePhotoUrl = (placeId, photoUrl) => {
+    const imagesDir = path.join(__dirname, '../../document_db', 'images');
+    if (!fs.existsSync(imagesDir)) {
+        fs.mkdirSync(imagesDir, { recursive: true });
+    }
+    const photoUrlFilePath = path.join(imagesDir, `${placeId}.txt`);
+    fs.writeFileSync(photoUrlFilePath, photoUrl);
 };
