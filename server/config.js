ec51f0182423d22a417887ac0f21da320b616639
ym-xu <yiming.tsui@gmail.com>
2024-06-04 14:53:01 +0800

    Remove API key from config

--- server/config.js
+++ server/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    googleApiKey: 'AIzaSyDAmhJ5qll0QM-HkIUe7DSuuhYu1pRKTUU',
+    googleApiKey: '',
     port: 3000,
-    openAiApiKey: "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",
-    openaiBaseURL: "https://gateway.ai.cloudflare.com/v1",
-    openaiEndpoint: "d4c6cd72f6192b6d992c8c58b4c67f09/goexpore/openai",
-    proxyHost: '127.0.0.1',
+    openAiApiKey: "",
+    openaiBaseURL: "",
+    openaiEndpoint: "",
+    proxyHost: '',
     proxyPort: 7890
 };
